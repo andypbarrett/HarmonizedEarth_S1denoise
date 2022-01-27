e427cc2df9e7854de789e6ce3b76c1704685b9a5
Andy Barrett <apbarret@nsidc.org>
2022-01-27 12:01:40 -0700

    add test to check initialized arrays

--- tests/test.py
+++ tests/test.py
@@ -7,6 +7,30 @@ DATAPATH = Path('/media', 'apbarret', 'andypbarrett_work', 'Data', 'Sentinel1_Su
 IMAGEPATH = DATAPATH / 'S1A_EW_GRDM_1SDH_20180902T165032_20180902T165132_023522_028FAA_35D2.zip'
 
 
-s1 = S1Image(str(IMAGEPATH))
-print(s1)
+def test_class_init():
+    '''Attempts to load image'''
+    try:
+        s1 = S1Image(IMAGEPATH)
+    except:
+        print(f'Failed to load {IMAGEPATH}')
+        #print(error)
+        return
+    
+    print(s1)
+    return s1
 
+
+def test_arrays_init(s1):
+    '''Checks initialized data arrays are empty'''
+
+    assert s1.nesz.size == 0, 'self.nesz is not empty'
+    assert s1.nesz_scaled.size == 0, 'self.nesz_scaled is not empty'
+    assert s1.sigma0.size == 0, 'self.sigma0 is not empty'
+    assert s1.sigma0_denoised.size == 0, 'self.sigma0_denoised is not empty'
+    
+    print(f'{test_arrays_init.__name__}: nesz, nesz_scaled, sigma0, sigma0_denoised all empty ndarrays')
+
+
+if __name__ == "__main__":
+    s1 = test_class_init()
+    test_arrays_init(s1)
