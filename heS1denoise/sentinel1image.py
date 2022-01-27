8656243a80d2ac8cb5ade6e0724b53c0b8f043b7
Andy Barrett <apbarret@nsidc.org>
2022-01-27 11:38:45 -0700

    add dummy vars for sigma0 and others arrays

--- heS1denoise/sentinel1image.py
+++ heS1denoise/sentinel1image.py
@@ -4,4 +4,14 @@ from s1denoise import Sentinel1Image
 
 
 class S1Image(Sentinel1Image):
-    pass
+    
+    def __init__(self, filename, mapper_name='sentinel1_l1', log_level=30):
+        super().__init__(str(filename), mapperName=mapper_name, logLevel=log_level)
+        self.sigma0 = []
+        self.nesz = []
+        self.nesz_scaled = []
+        self.sigma0_denoised = []
+
+
+    def calculate_scaling_factors(self):
+        pass
