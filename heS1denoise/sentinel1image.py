0dbdf90704714121fdfb7cc33076cbcf6d0b1ea3
Andy Barrett <apbarret@nsidc.org>
2022-01-27 12:01:19 -0700

    initialize data arrays to empty numpy arrays

--- heS1denoise/sentinel1image.py
+++ heS1denoise/sentinel1image.py
@@ -1,5 +1,7 @@
 '''Classes and methods to denoise Sentinel1 images'''
 
+import numpy as np
+
 from s1denoise import Sentinel1Image
 
 
@@ -7,10 +9,10 @@ class S1Image(Sentinel1Image):
     
     def __init__(self, filename, mapper_name='sentinel1_l1', log_level=30):
         super().__init__(str(filename), mapperName=mapper_name, logLevel=log_level)
-        self.sigma0 = []
-        self.nesz = []
-        self.nesz_scaled = []
-        self.sigma0_denoised = []
+        self.sigma0 = np.array([])
+        self.nesz = np.array([])
+        self.nesz_scaled = np.array([])
+        self.sigma0_denoised = np.array([])
 
 
     def calculate_scaling_factors(self):
